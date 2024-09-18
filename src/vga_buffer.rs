3fa22d66ac1218d541fe0d33516e0c0021ccc876
RazerBlade6 <vedant.thapar@gmail.com>
2024-09-18 11:52:17 +0400

    Completed VGA Buffer Writer

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,6 +1,4 @@
-use core::arch::global_asm;
-
-#[allow(dead_code)]
+#![allow(unused)]
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[repr(u8)]
 pub enum Color {
@@ -22,21 +20,77 @@ pub enum Color {
     White      = 0xf,
 }
 
-#[repr(transparent)]
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[repr(C)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct ColorCode(u8);
 
 impl ColorCode {
     fn new(foreground: Color, background: Color) -> Self {
-        Self(background as u8)
+        Self((background  as u8) << 4 | foreground as u8)
     }
 }
 
+#[repr(C)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Screen {
     ascii_char: u8,
     color: ColorCode
 }
 
-const BUFFER_HEIGHT: u8 = 25;
-const BUFFER_WIDTH: u8 = 80;
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
 
+#[repr(transparent)]
+struct Buffer {
+    chars: [[Screen; BUFFER_WIDTH]; BUFFER_HEIGHT]
+}
+
+pub struct Writer {
+    column: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer
+}
+
+
+impl Writer {
+    pub fn new(foreground: Color, background: Color) -> Self {
+        Self {
+            column: 0,
+            color_code: ColorCode::new(foreground, background),
+            buffer: unsafe { &mut *(0xb8000 as *mut Buffer)}
+        }
+    }
+
+    pub fn write(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match &byte {
+                0x20..= 0x70 | b'\n' => self.write_byte(byte),
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+
+    fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => {
+                self.newline();
+            }
+            other => {
+                if self.column >= BUFFER_WIDTH {
+                    self.newline();
+                }
+                
+                self.buffer.chars[BUFFER_HEIGHT - 1][BUFFER_WIDTH] = Screen {
+                    ascii_char: byte,
+                    color: self.color_code
+                };
+
+                self.column += 1;
+            } 
+        }
+    }
+
+    fn newline(&mut self) {
+        todo!();
+    }
+}
