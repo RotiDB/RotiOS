4a753772705f55c616f8d9c55e76614caff635d0
RazerBlade6 <vedant.thapar@gmail.com>
2024-10-02 10:26:59 +0400

    Added a tour, and also some more cosmetic stuff

--- src/bin/uefi.s
+++ src/bin/uefi.s
@@ -1,7 +1,8 @@
+bits 64
+
 %include "./src/bin/efi_defs.s"
 %include "./src/bin/efi_boot_services.s"
-
-bits 64
+%include "./src/bin/efi_text_output_services.s"
 
 section .text
 global  start
@@ -10,9 +11,9 @@ start:
     mov [ptrUEFI], rsp
 
     sub rsp, 4 * 8
-    mov [imageHandle], rbx
+    mov [imageHandle], rcx
 
-    mov [ptrSystemTable], rax
+    mov [systemTable], rdx
     
     ; In theory, this should be all we have to do
     ; Should probably include a verification step
@@ -38,41 +39,32 @@ exitBootSequence:
     call exitBootServices
     ret
 
-loadKernel:  
-    mov  rcx, [ptrSystemTable]
-    and  rdx, 0
- 
-    mov  r8, 2048 * 100
-    call kernelActions 
-    ret
+loadKernel:
+    call clearScreen
+    lea  rcx, string 
+    call outputString
+    
+    call exitKernel
 
-kernelActions:
-    mov  rcx, [rcx + EFI_SYSTEM_TABLE.ConOut]
-    EFI_TEXT_ATTR rbx, EFI_BACKGROUND_BLACK, EFI_GREEN
-    mov  rcx, [rcx + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.SetAttribute]
-
-    call rcx
-    ret
-
+EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID:        dd 0x9042a9de
+                                          dd 0x23dc4a38
+                                          dd 0x96fb7ade
+                                          dd 0xd080516a
 
 section .data
         string                            db   __utf16__ 'Hello World', 13, 10, 13, 10
         exitString                        db   __utf16__ 'Press any key to shutdown...'
         ptrUEFI                           dq   0
         imageHandle                       dq   0
-        ptrSystemTable                    dq   0
+        systemTable                       dq   0
         ptrInterface                      dq   0
 
-        intMemoryMapSize                  dq   EFI_MEMORY_DESCRIPTOR_size * 1024
-        bufMemoryMap                      resb EFI_MEMORY_DESCRIPTOR_size * 1024
-        ptrMemoryMapKey                   dq   0
-        ptrMemoryMapDescSize              dq   0
+        memoryMapSize                     dq   EFI_MEMORY_DESCRIPTOR_size * 1024 ; reserving 1kb of memory at this time
+        memoryMap                         resb EFI_MEMORY_DESCRIPTOR_size * 1024
+        mapKey                            dq   0
+        descriptorSize                    dq   0
+        descriptorVersion                 dd   0
 
-        EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID dd 0x9042a9de
-                                          dd 0x23dc4a38
-                                          dd 0x96fb7ade
-                                          dd 0xd080516a
-
-        ptrFrameBuffer              dq 0
+        frameBuffer              dq 0
 datasize equ $ - $$
 
